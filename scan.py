35ff73366707e16e8e51d708d14dfa47736ee2fe
vanbay01nd <67738460+vanbay01nd@users.noreply.github.com>
2024-06-25 10:53:15 +0700

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,120 +1,104 @@
-import os
-import random
-import time
-import requests
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from hdwallet import HDWallet
+from colorama import Fore, init  
+from hdwallet import HDWallet  
 from hdwallet.symbols import BTC, ETH
-from colorama import Fore, init
-from pyfiglet import Figlet
+import random  
+import requests  
+import os  
+import time  
+import platform  
+from pyfiglet import Figlet  
 
 init(autoreset=True)
 
 def title():
-    f = Figlet(font='standard')
+    f = Figlet(font='standard')  
     print(Fore.LIGHTCYAN_EX + f.renderText("Cryptonix") + Fore.RESET)
 
-def clear_screen():
-    os.system('clear' if os.name == 'posix' else 'cls')
+def get_clear():
+    if 'win' in platform.platform().lower():
+        os.system('cls')
+    elif 'linux' in platform.platform().lower() or 'darwin' in platform.platform().lower():
+        os.system('clear')
+    else:
+        raise ValueError('Not Supported Platform: "%s"' % platform.platform())
 
-def eth_balance(addr: str, session: requests.Session) -> float:
+def eth_balance(addr: str) -> float:
     url = f"https://ethereum.atomicwallet.io/api/v2/address/{addr}"
     try:
-        req = session.get(url).json()
+        req = requests.get(url).json()
         ret = req['balance']
         return int(ret) / 1e18
     except (KeyError, requests.RequestException) as e:
         print(f"Error fetching Ethereum balance: {e}")
         return 0.0
 
-def btc_balance(addr: str, session: requests.Session) -> float:
+def get_balance(addr: str) -> float:
     url = f"https://bitcoin.atomicwallet.io/api/v2/address/{addr}"
     try:
-        req = session.get(url).json()
+        req = requests.get(url).json()
         ret = req['balance']
         return int(ret) / 1e8
     except (KeyError, requests.RequestException) as e:
         print(f"Error fetching Bitcoin balance: {e}")
         return 0.0
 
-def check_wallet(private_key):
-    session = requests.Session()
-    hd_btc = HDWallet(symbol=BTC)
-    hd_eth = HDWallet(symbol=ETH)
-    hd_btc.from_private_key(private_key)
-    hd_eth.from_private_key(private_key)
-
-    eth_addr = hd_eth.p2pkh_address()
-    btc_addrs = {
-        "P2PKH": hd_btc.p2pkh_address(),
-        "BECH32": hd_btc.p2wpkh_address(),
-        "P2WPKH": hd_btc.p2wpkh_in_p2sh_address(),
-        "P2WSH": hd_btc.p2wsh_in_p2sh_address(),
-        "P2SH": hd_btc.p2sh_address()
-    }
-
-    btc_balances = {}
-    for addr_type, addr in btc_addrs.items():
-        btc_balances[addr_type] = btc_balance(addr, session)
-    eth_balance_value = eth_balance(eth_addr, session)
-
-    return private_key, btc_addrs, eth_addr, btc_balances, eth_balance_value
-
 def main():
-    clear_screen()
+    get_clear()  
     title()
-    print(Fore.GREEN + "Starting..." + Fore.RESET)
-    time.sleep(2)
+    print(Fore.GREEN, "Starting...", Fore.RESET)  
+    time.sleep(2)  
 
-    total_scans = 0
-    found_wallets = 0
-
-    max_workers = 7  # Chỉ định 7 luồng
-
-    with ThreadPoolExecutor(max_workers=max_workers) as executor:
-        futures = []
+    z = 1  
+    ff = 0  
+    while True:
         try:
-            while True:
-                private_key = "".join(random.choice("0123456789abcdef") for _ in range(64))
-                futures.append(executor.submit(check_wallet, private_key))
+            private_key = "".join(random.choice("0123456789abcdef") for _ in range(64))
+            hd_btc = HDWallet(symbol=BTC)
+            hd_eth = HDWallet(symbol=ETH)
+            hd_btc.from_private_key(private_key)
+            hd_eth.from_private_key(private_key)
 
-                for future in as_completed(futures):
-                    clear_screen()
-                    title()
-                    total_scans += 1
+            eth_addr = hd_eth.p2pkh_address()
+            btc_addrs = {
+                "P2PKH": hd_btc.p2pkh_address(),
+                "BECH32": hd_btc.p2wpkh_address(),
+                "P2WPKH": hd_btc.p2wpkh_in_p2sh_address(),
+                "P2WSH": hd_btc.p2wsh_in_p2sh_address(),
+                "P2SH": hd_btc.p2sh_address()
+            }
 
-                    private_key, btc_addrs, eth_addr, btc_balances, eth_balance_value = future.result()
+            balances = {addr_type: get_balance(addr) for addr_type, addr in btc_addrs.items()}
+            eth_balance_value = eth_balance(eth_addr)
 
-                    print(Fore.YELLOW + "Discord: cr0mbleonthegame" + Fore.RESET)
-                    print(f"Scan: {total_scans} Found: {found_wallets}\n")
+            get_clear()
+            title()
 
+            print(Fore.YELLOW, "Discord: cr0mbleonthegame", Fore.RESET)
+            print(f"Scan: {z} Found: {ff}")
+
+            for addr_type, addr in btc_addrs.items():
+                balance = balances[addr_type]
+                print(f"{Fore.WHITE}BTC Address ({addr_type}) | BAL: {Fore.MAGENTA}{balance} | {Fore.YELLOW}{addr}")
+
+            print(f"{Fore.WHITE}ETH Address (ETH)    | BAL: {Fore.MAGENTA}{eth_balance_value} | {Fore.YELLOW}{eth_addr}")
+            print(f"{Fore.WHITE}Private Key (HEX)    | {Fore.MAGENTA}{private_key}")
+            print("=" * 70)
+
+            z += 1  
+
+            if any(balance > 0 for balance in balances.values()) or eth_balance_value > 0:
+                ff += 1
+                with open('btcWin.txt', 'a') as f:
                     for addr_type, addr in btc_addrs.items():
-                        btc_balance = btc_balances.get(addr_type, 0.0)
-                        print(f"{Fore.WHITE}BTC Address ({addr_type}) | BAL: {Fore.MAGENTA}{btc_balance} | {Fore.YELLOW}{addr}")
-
-                    print(f"{Fore.WHITE}ETH Address (ETH)    | BAL: {Fore.MAGENTA}{eth_balance_value} | {Fore.YELLOW}{eth_addr}")
-                    print(f"{Fore.WHITE}Private Key (HEX)    | {Fore.MAGENTA}{private_key}")
-                    print("=" * 70)
-
-                    if any(balance > 0 for balance in btc_balances.values()) or eth_balance_value > 0:
-                        found_wallets += 1
-                        with open('found_wallets.txt', 'a') as f:
-                            f.write(f'Scan: {total_scans}, Found: {found_wallets}\n')
-                            for addr_type, addr in btc_addrs.items():
-                                btc_balance = btc_balances.get(addr_type, 0.0)
-                                if btc_balance > 0:
-                                    f.write(f'BTC Address ({addr_type}): {addr}\nBalance: {btc_balance}\n')
-                            if eth_balance_value > 0:
-                                f.write(f'ETH Address: {eth_addr}\nBalance: {eth_balance_value}\n')
-                            f.write(f'Private Key: {private_key}\n\n')
-
-                    futures = [f for f in futures if not f.done()]
+                        if balances[addr_type] > 0:
+                            f.write(f'{addr}\n{private_key}\n')
+                    if eth_balance_value > 0:
+                        f.write(f'{eth_addr}\n{private_key}\n')
 
         except Exception as e:
             print(f"An error occurred: {e}")
             print("Restarting in 5 seconds...")
             time.sleep(5)
-            main()
 
 if __name__ == "__main__":
     main()
